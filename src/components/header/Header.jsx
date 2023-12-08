847576d37a97d36481b92d6d883aace6dc67d9b0
Dorszyk11 <tymbeixpoi@gmail.com>
2023-12-08 11:03:59 +0100

    2

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css';
+
+const Header = () => {
+    return(
+        <div className="container header">
+            <h1>Ściąga na <span>| Historie</span></h1>
+            <p>Reformacja i kontrreformacja w polsce</p>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/db/topics.js
@@ -0,0 +1,40 @@
+export const Topics = [
+  {
+    id: 1,
+    temat: "Narastające niezadowolenie społeczeństwa wobec Kościoła katolickiego",
+    opis: "Podobnie jak to miało miejsce w wielu innych krajach, również w Królestwie Polskim i Wielkim Księstwie Litewskim uwidoczniła się w XVI wieku duża niechęć znacznej części ludności do Kościoła. Przyczyn tego stanu rzeczy było wiele. Niewygodna dla tych, którzy żyli z rolnictwa była pobierana przez Kościół dziesięcina. Raziło wielkie bogactwo Kościoła i jego najwyższych dostojników. Nie podobał się sposób życia duchownych. Bardzo krytycznie oceniano ich poziom intelektualny. Drażniły uzyskiwane przez Kościół przywileje, zwłaszcza te, które zwalniały duchowieństwo z obowiązku płacenia podatków. Strach budziły uprawnienia sądów kościelnych i ich postępowanie wobec osób świeckich. Krytykowano posługiwanie się podczas odprawiania nabożeństw i sporządzania dokumentów kościelnych niezrozumiałą dla wiernych łaciną. Coraz częściej domagano się tłumaczenia Biblii na język polski.",
+    daty: ``
+  },
+  {
+    id: 2,
+    temat: "Ruchy reformacyjne na ziemiach polskich",
+    opis: `Te i inne czynniki powodowały, że działania reformacyjne zyskały na ziemiach polskich dużą popularność. Religijne „nowinki” przyjmowały się zarówno wśród mieszczan, jak i wśród szlachty. Podatni na nie byli też niektórzy księża i zakonnicy.
+    Nauka Lutra rozpowszechniła się na dużą skalę na terenie Prus Królewskich i w Wielkopolsce. Jej zwolenników nie brakowało też w innych regionach kraju.
+    Jeszcze większą popularność zyskał sobie zarówno w Polsce, jak i na Litwie kalwinizm. W połowie XVI wieku znaczna część szlachty upodobała sobie to wyznanie, odchodząc od idei niemieckiego luteranizmu. Można było już wtedy dostrzec oznaki kształtowania się Kościoła kalwińskiego w Polsce. Wyznanie to stało się też bliskie tzw. „braciom czeskim”. Byli to przybyli z Czech zwolennicy czeskiego reformatora Jana Husa, którego za podejmowane próby naprawy Kościoła spalono w 1415 roku na stosie.`
+    },
+  {
+    id: 3,
+    temat: "Kontreformy kościoła",
+    opis:`
+      W XVI w był ruch egzekucyjny czyli szlachta wymiśliła że magnaci mają za dużo ziemi i że mają to oddać królowi.
+      Majątki króla zostały obłorzone podatkiem(25%) i poszło to na wojsko
+      Bona została królową polski - wróciła z renesansowych włoch - i zaczęła zmiany:
+      - przywiozła i rozpopularyzowała włoszczyzne, pomidory, widelec
+      - zprowadziła artystów którzy rysowali portrety(równe proporcje)
+      Rozwój szkolenictwa
+
+      Traktat o dwóch sarmacjach opisywał ziemie która "należy" do szlachty ale w XVI w nikt w to nie uwierzył ale w XVII w już przez wojny społeczeństwo zgłupiało i uwierzyli w tą księgie która sporo namieszała
+      Mikołaj rej rozpowszechnił literature w języku narodowim
+      Arasy - rzadkie tkaniny na ściane ukradzione przez niemców ",
+      daty: "1603 - śmierć Elżbiety I (bez potomnie bo nie miała ślubu) | 1603-1625 - Jakub I Stuart | 1625-1649 - Karol I Stuart | Jak karolowi się zbuntował Londyn to ten uciekł do szwecji a tam byli innego wyznania więc go zcieli | Awans za zasługi - jak brałeś udział w akcji która zabijała kogoś ważnego to dostawałeś awans w wrmi | Oliver Cromwell - ztworzył armie nowego wzoru w 1644 i wszystkich rozgniatał | 1685 - Jakub II | 1689 - Wilhelm III
+  `,
+  daty: ``
+  },
+  {
+    id: 4,
+    temat: "Kościół kalwiński w latach 1562-1565",
+    opis: `W latach 1562-1565 doszło do podziału polskiego Kościoła kalwińskiego. Wyodrębnił się wtedy zbór większy kalwiński i zbór mniejszy ariański. Arianie, których nazywano też „braćmi polskimi” nawiązywali w swoich naukach do początków chrześcijaństwa. Sprzeciwiali się podstawowym dogmatom, czyli prawdom głoszonym przez Kościół katolicki. Nie uznawali przede wszystkim dogmatu o Trójcy Świętej. Opowiadali się za udzielaniem sakramentu chrztu osobom dorosłym. Byli zwolennikami równości i sprawiedliwości społecznej oraz przeciwnikami jakichkolwiek wojen.
+    Luteranizm i kalwinizm objęły swoim zasięgiem znaczną część ludności. Nowe wyznanie przyjęli nie tylko liczni mieszczanie, ale też mniej więcej co szósty szlachcic. Wśród protestantów byli przedstawiciele najznakomitszych polskich i litewskich rodów magnackich.`,
+    daty: ``
+  },
+]
